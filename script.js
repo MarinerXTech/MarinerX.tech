214e4345a5a60e3212722e924c14062f821438be
MarinerXTech <Marinerxtech@gmail.com>
2026-04-20 07:29:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getCurrentPageName() {
 // Vacation shipping notice toggle.
 // Set enabled to false when normal shipping resumes, then update shipDateLabel for future trips.
 const VACATION_NOTICE = {
-  enabled: true,
+  enabled: false, // Change back to true and update shipDateLabel for a future trip.
   shipDateLabel: 'Monday, April 20, 2026'
 };
 

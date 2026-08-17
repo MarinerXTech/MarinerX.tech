db59e244d15e1857ad154a818ad9bc1acfacdd8d
Drew <marinerxtech@gmail.com>
2026-08-16 23:32:07 -0400

    Update sail ring hanger price to $19.95

--- catalog.js
+++ catalog.js
@@ -87,13 +87,13 @@ window.PRODUCT_CATALOG = {
   // Sail rings + mainsheet hanger SKUs
   'rings-hanger-5pk-white': {
     name: 'Sail Rings + Mainsheet Hanger (5 Pack) - White',
-    price: 13.95,
+    price: 19.95,
     category: 'Rigging Accessories',
     image: { src: 'images/rings_hanger_combo_3.jpg', alt: 'Sail Rings + Hanger White' }
   },
   'rings-hanger-5pk-teal': {
     name: 'Sail Rings + Mainsheet Hanger (5 Pack) - Teal',
-    price: 13.95,
+    price: 19.95,
     category: 'Rigging Accessories',
     image: { src: 'images/rings_hanger_combo_1.jpg', alt: 'Sail Rings + Hanger Teal' }
   },
